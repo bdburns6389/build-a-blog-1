022e371e32715ff67181ba97544152c0a53be0e7
Shireen Korkzan <skzan15@sbcglobal.net>
2017-12-16 11:42:52 -0500

    Edited quotation error in line one of index.html and added import os in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 import jinja2
+import os
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
