5ffbf351e09e14e8907c0b9e35d340f053b58235
Shireen Korkzan <skzan15@sbcglobal.net>
2017-12-18 00:35:37 -0500

    Fixed newpost in html and code

--- main.py
+++ main.py
@@ -25,24 +25,33 @@ class Blog(db.Model):
 def index():
 
     blogs = Blog.query.all()
-    return render_template('index.html',title="build-a-blog",
+    print(blogs)
+    print('hello')
+    return render_template('blog.html',title="build-a-blog",
         blogs=blogs)
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
 
     if request.method == 'POST':
-        blog_title = request.form['title']
-        if not form.validate():
+        print ("hello")
+        blog_title = request.form['blog_title']
+        print(blog_title)
+        if len(blog_title) <= 0:
+            print("Empty blog title")
             blog_title_error = "Please include a blog title"
             return render_template('newpost.html', form=form, blog_title_error=blog_title_error)
-        blog_body = request.form['body']
-        if not blog_body.validate():
+        print("Good blog title")
+        blog_body = request.form['blog_body']
+        if len(blog_body) <= 0:
+            print("Empty blog body")
             body_error = "Please include a post body"
             return render_template('newpost.html', form=form, body_error=body_error)
+        print("Good blog body")
         new_blog = Blog(blog_title, blog_body)
         db.session.add(new_blog)
         db.session.commit()
+        print("After post committed")
         return redirect('/blog?id={}'.format(new_blog.id))
     else:
         return render_template('newpost.html')
@@ -51,10 +60,9 @@ def newpost():
 def blog():
 
     blog_id = int(request.args.get('id'))
-    blog = blogs.query.get(id=blog_id)
+    blog = Blog.query.filter_by(id=blog_id).first()
 
-    return render_template('blog.html', blog=blog_id)
-
+    return render_template('titlebody.html', blog=blog)
 
 if __name__ == '__main__':
     app.run()
