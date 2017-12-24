ffa3fa0c4236ea7fb984fc54b745cf5b4222574f
Brian Burns <bdburns6389@gmail.com>
2017-12-24 13:54:54 -0600

    cleaning up imports

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
-import jinja2
-import os
 
-template_dir = os.path.join(os.path.dirname(__file__), "templates")
-jinja = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:root@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'root'
 
 class Blog(db.Model):
 
@@ -25,8 +24,6 @@ class Blog(db.Model):
 def index():
 
     blogs = Blog.query.all()
-    print(blogs)
-    print('hello')
     return render_template('blog.html',title="build-a-blog",
         blogs=blogs)
 
@@ -34,24 +31,22 @@ def index():
 def newpost():
 
     if request.method == 'POST':
-        print ("hello")
         blog_title = request.form['blog_title']
-        print(blog_title)
+        blog_body = request.form['blog_body']
+        
         if len(blog_title) <= 0:
-            print("Empty blog title")
             blog_title_error = "Please include a blog title"
             return render_template('newpost.html', form=form, blog_title_error=blog_title_error)
-        print("Good blog title")
-        blog_body = request.form['blog_body']
+        
+        
         if len(blog_body) <= 0:
-            print("Empty blog body")
             body_error = "Please include a post body"
             return render_template('newpost.html', form=form, body_error=body_error)
-        print("Good blog body")
+
         new_blog = Blog(blog_title, blog_body)
         db.session.add(new_blog)
         db.session.commit()
-        print("After post committed")
+
         return redirect('/blog?id={}'.format(new_blog.id))
     else:
         return render_template('newpost.html')
